bebbad756fb387ffb814d94dfcbac5a0bd9fe8ae
DESKTOP-NUJTJD3\Jouhouken <zoro18782ryt131@gmail.com>
2019-09-25 16:55:59 +0900

    動作するように

---
+++ 2019 VRgame/Assets/myfolder/script/SingletonMonobehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonobehaviour
+{
+    private static SingletonMonobehaviour instance;
+    
+    public static SingletonMonobehaviour Instance
+    {
+        get
+        {
+            if (instance == null) instance = new SingletonMonobehaviour();
+            return instance;
+        }
+    }
+}
