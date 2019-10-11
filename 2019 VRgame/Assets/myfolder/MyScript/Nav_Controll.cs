e66857ca24934b282959f0f44d767eed8a406248
DESKTOP-NUJTJD3\Jouhouken <zoro18782ryt131@gmail.com>
2019-10-11 14:48:42 +0900

    次の目的地をパーティクルで表示する機能を追加

--- 2019 VRgame/Assets/myfolder/MyScript/Nav_Controll.cs
+++ 2019 VRgame/Assets/myfolder/MyScript/Nav_Controll.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class Nav_Controll : MonoBehaviour
 {
-    public Transform[] destinations;
+    public GameObject[] destinations;
     [SerializeField] NavMeshAgent navMesh = null;
     [SerializeField] Transform Player;
     private int idx = 0;
@@ -16,10 +16,18 @@ public class Nav_Controll : MonoBehaviour
     private bool isPlaying;
     public Text debugtext;
 
+    private ParticleSystem particle;
+
     // Start is called before the first frame update
     void Start()
     {
         isPlaying = false;
+        destinations = GameObject.FindGameObjectsWithTag("Destinations");
+        foreach(GameObject obj in destinations)
+        {
+            var child = obj.transform.GetChild(0).gameObject;//パーティクルのオブジェクトを取得
+            child.GetComponent<ParticleSystem>().Stop();//最初は全部非表示
+        }
     }
 
     // Update is called once per frame
@@ -32,6 +40,8 @@ public class Nav_Controll : MonoBehaviour
             isPlaying = !isPlaying;
         }
         debugtext.text = "state:" + isPlaying.ToString();
+
+
         if (!isPlaying)
         {
             navMesh.velocity = Vector3.zero;
@@ -47,17 +57,30 @@ public class Nav_Controll : MonoBehaviour
                 navMesh.isStopped = true;
                 return;
             }
-            if (Vector3.Distance(Player.position, destinations[idx].position) < 0.8f)
+
+            if (Vector3.Distance(Player.position, destinations[idx].transform.position) < 0.7f)
             {
+                particle = destinations[idx].transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+                if (particle.isPlaying)
+                {
+                    particle.Stop();//前のパーティクルをストップ
+                }
                 idx++;
             }
+
             if (idx >= destinations.Length)
             {
                 navMesh.velocity = Vector3.zero;
                 navMesh.isStopped = true;
                 return;
             }
+
             navMesh.SetDestination(destinations[idx].transform.position);
+            particle = destinations[idx].transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+            if (!particle.isPlaying)
+            {
+                particle.Play();
+            }
             //Debug.Log(idx);
         }
     }
